29a7e0a1142c50cac849a9b1eaaf9c7db8d15025
Bill-piu <tranthanhsang150719@gmail.com>
2025-06-09 00:26:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function createFlyingLetter() {
 function CreateFlingimg(){
 
   const urll=["img/3.jpg","img/4.jpg","img/6.jpg","img/9.jpg","img/10.jpg","img/11.jpg",
-  "img/12.jpg","img/13.jpg","img/14.jpg","img/16.jpg","img/17.jpg","img/20.jpg","img/21.jpg","img/22.jpg"]
+  "img/12.jpg","img/13.jpg","img/14.jpg","img/16.jpg","img/17.jpg","img/20.jpg","img/21.jpg","img/22.jpg",,"img/19.jpg"]
   const img = document.createElement("img");
   img.src=urll[(Math.floor(Math.random() * urll.length))];
   img.classList.add('imgg');
